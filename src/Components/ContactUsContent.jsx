971fe38c4a946be351f2dba8c775cf493baa98de
Elizabeth <lizbeepmc@gmail.com>
2024-03-17 02:41:01 +0300

    contact us done

--- src/Components/ContactUsContent.jsx
+++ src/Components/ContactUsContent.jsx
@@ -2,6 +2,7 @@
 import facebook from "../../assets/facebook.jpg";
 import tiktok from "../../assets/tiktok.jpg";
 import instagram from "../../assets/instagram.jpg";
+import whatsapp from "../../assets/whatsapp.jpg";
 
 import "./contactus.css"; // Style as needed
 
@@ -9,16 +10,24 @@ function ContactUsContent() {
   return (
     <div className="imagetextfooter">
       <div className="footer">
-        <h2>MEET US ON SOCIAL MEDIA</h2>
-        <a href="https://www.facebook.com/dinas.face.cream?mibextid=ZbWKwL">
-          <img src={facebook} className="facebook_icon" alt="Facebook" />
-        </a>
-        <a href="https://tiktok.com/@dinas_face_cream">
-          <img src={tiktok} className="tiktok_icon" alt="TikTok" />
-        </a>
-        <a href="https://instagram.com/dinas_face_cream">
-          <img src={instagram} className="instagram_icon" alt="Instagram" />
-        </a>
+        <div className="chat">
+          <h2>CHAT WITH US</h2>
+          <a href="https://wa.me/2348071971717" target="_blank">
+            <img src = {whatsapp}  className="whatsapp_icon" alt= "whatsapp"/>
+          </a>
+        </div>
+        <div className="meet">
+          <h2>MEET US ON SOCIAL MEDIA</h2>
+          <a href="https://www.facebook.com/dinas.face.cream?mibextid=ZbWKwL">
+            <img src={facebook} className="facebook_icon" alt="Facebook" />
+          </a>
+          <a href="https://tiktok.com/@dinas_face_cream">
+            <img src={tiktok} className="tiktok_icon" alt="TikTok" />
+          </a>
+          <a href="https://instagram.com/dinas_face_cream">
+            <img src={instagram} className="instagram_icon" alt="Instagram" />
+          </a>
+        </div>
       </div>
     </div>
   );
